d241c2f216c31b40017098b80fe9f22d00e7af6c
Lee Jaeung <62253743+jaeung-E@users.noreply.github.com>
2023-03-16 18:51:50 +0900

    [level 1] Title: 삼총사, Time: 0.46 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -0,0 +1,18 @@
+function solution(number) {
+    return combination(number, 3)
+        .filter((arr) => arr.reduce((sum, num) => sum += num, 0) === 0)
+        .length;
+}
+
+function combination(arr, num){
+    const res = [];
+    if(num === 1) return arr.map((v) => [v]);
+    arr.forEach((v, idx, arr) => {
+        const rest = arr.slice(idx+1);
+        const combinations = combination(rest, num-1);
+        const attach = combinations.map((combination) => [v, ...combination]);
+        res.push(...attach);
+    });
+
+    return res;
+}
