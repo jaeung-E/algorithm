a08c0ca3555b0cdee77138e40565dcd2bd4234a8
Lee Jaeung <62253743+jaeung-E@users.noreply.github.com>
2023-04-17 18:07:22 +0900

    [unrated] Title: 귤 고르기, Time: 37.33 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -1,6 +1,5 @@
 function solution(k, tangerine) {
     let answer = 0;
-    let prevSize = 0;
     const tangerineMap = new Map();
     
     tangerine.forEach((t) => tangerineMap.set(t, (tangerineMap.get(t) ?? 0) + 1));
@@ -9,7 +8,7 @@ function solution(k, tangerine) {
         .sort((a, b) => b[1] - a[1])
         .some(([size, count]) => {
             k -= count;
-            if (prevSize !== size) answer++;
+            answer++;
             if (k <= 0) return true;
         });
         
