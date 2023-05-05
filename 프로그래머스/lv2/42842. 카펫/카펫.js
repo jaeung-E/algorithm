97530a0ecc3903d8ce1fbdec7b2e669c5fa0f174
Lee Jaeung <62253743+jaeung-E@users.noreply.github.com>
2023-05-05 21:45:01 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -0,0 +1,9 @@
+function solution(brown, yellow) {
+    for (let i = 1; i <= yellow; i++) {
+        if (yellow % i !== 0) continue;
+        
+        const width = yellow / i + 2;
+        const height = i + 2;
+        if (width * height - yellow === brown) return [width, height];
+    }
+}
