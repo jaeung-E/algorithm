21ba7a00f99cec23ab8378eac424865687633e1a
Lee Jaeung <62253743+jaeung-E@users.noreply.github.com>
2023-05-20 00:22:22 +0900

    [unrated] Title: 무인도 여행, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.js
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.js
@@ -5,7 +5,7 @@ function solution(maps) {
     for (let i = 0; i < maps.length; i++) {
         for (let j = 0; j < maps[i].length; j++) {
             if (!visited[i][j] && maps[i][j] !== 'X') {
-                answer.push(BFS(i, j, maps, visited));
+                answer.push(DFS(i, j, maps, visited));
             }
         }
     }
@@ -15,25 +15,18 @@ function solution(maps) {
     return answer.length ? answer : [-1];
 }
 
-function BFS(x, y, maps, visited) {
+function DFS(x, y, maps, visited) {
     let sum = +maps[x][y];
-    const queue = [[x, y]];
     const dx = [-1, 0, 1, 0];
     const dy = [0, 1, 0, -1];
     visited[x][y] = true;
 
-    while (queue.length) {
-        const [X, Y] = queue.shift();
+    for (let i = 0; i < 4; i++) {
+        const nx = x + dx[i];
+        const ny = y + dy[i];
 
-        for (let i = 0; i < 4; i++) {
-            const nx = X + dx[i];
-            const ny = Y + dy[i];
-
-            if (isBound(nx, ny, maps) && !visited[nx][ny] && maps[nx][ny] !== "X") {
-              visited[nx][ny] = true;
-              sum += +maps[nx][ny];
-              queue.push([nx, ny]);
-            }
+        if (isBound(nx, ny, maps) && !visited[nx][ny] && maps[nx][ny] !== "X") {
+            sum += DFS(nx, ny, maps, visited)
         }
     }
     
