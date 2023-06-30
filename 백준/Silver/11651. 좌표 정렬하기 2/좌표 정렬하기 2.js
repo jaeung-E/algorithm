eb2135f42d4db60f2774e3361dc7ce4c04372ac7
Lee Jaeung <62253743+jaeung-E@users.noreply.github.com>
2023-06-30 23:36:01 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 744 ms, Memory: 72128 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+const [[_], ...coor] = input.map((str) => str.split(" ").map(Number));
+
+function solution() {
+  let result = "";
+
+  coor
+    .sort(([x1, y1], [x2, y2]) => {
+      if (y1 === y2) return x1 - x2;
+      return y1 - y2;
+    })
+    .forEach(([x, y]) => (result = result.concat(`${x} ${y}\n`)));
+
+  return result;
+}
+
+console.log(solution());
